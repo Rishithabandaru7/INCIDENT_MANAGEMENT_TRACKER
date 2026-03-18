5f3ea07ebfce4ca9fcaf48d6c1e75f064a26e5e4
bandarurishitha <bandarurishitha0407@gmail.com>
2026-03-18 11:51:18 +0530

    updated

--- db.py
+++ db.py
@@ -1,19 +1,22 @@
+import time
 import mysql.connector
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-# Load .env from the same folder as db.py
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
-def get_conn():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user=os.getenv("DB_USER"),                    
-        password=os.getenv("DB_PASSWORD"),  
-        database=os.getenv("DB_NAME")    
-    )
-    return conn
-print("DB_USER:", os.getenv("DB_USER"))
-print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
-print("DB_NAME:", os.getenv("DB_NAME"))
+def get_conn(retries=5, delay=3):
+    for i in range(retries):
+        try:
+            conn = mysql.connector.connect(
+                host=os.getenv("DB_HOST","mysql"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME"),
+                port=3306
+            )
+            return conn
+        except Exception as e:
+            print(f"DB not ready, retry {i+1}/{retries}: {e}")
+            time.sleep(delay)
+    raise Exception("Cannot connect to DB after retries")
